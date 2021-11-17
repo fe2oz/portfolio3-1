dedf835f752c4ac1b2140240dfb8d52553acc954
fe2oz <93234748+fe2oz@users.noreply.github.com>
2021-11-17 16:58:54 +0900

    11.17++

--- js/one_section_text1_change.js
+++ js/one_section_text1_change.js
@@ -2,7 +2,43 @@
 
 
     $(function(){
-        $('.nv2 li:nth-of-type(1)').click(function(){
-            
-        });
+        $('.nv li:nth-of-type(1)').click(function(){
+            $(".changebox2").css("opacity", "1");
+            $('.changebox1').css("opacity", "0");
+            $('.changebox3').css("opacity", "0");
+            $('.changebox4').css("opacity", "0");
+            $('.changebox5').css("opacity", "0");
+        })
+
+        $('.nv li:nth-of-type(2)').click(function(){
+            $(".changebox3").css("opacity", "1");
+            $('.changebox1').css("opacity", "0");
+            $('.changebox2').css("opacity", "0");
+            $('.changebox4').css("opacity", "0");
+            $('.changebox5').css("opacity", "0");
+        })
+
+        $('.nv li:nth-of-type(3)').click(function(){
+            $('.changebox1').css("opacity", "1");
+            $('.changebox2').css("opacity", "0");
+            $('.changebox3').css("opacity", "0");
+            $('.changebox4').css("opacity", "0");
+            $('.changebox5').css("opacity", "0");
+        })
+
+        $('.nv li:nth-of-type(4)').click(function(){
+            $('.changebox4').css("opacity", "1");
+            $('.changebox1').css("opacity", "0");
+            $('.changebox2').css("opacity", "0");
+            $('.changebox3').css("opacity", "0");
+            $('.changebox5').css("opacity", "0");
+        })
+
+        $('.nv li:nth-of-type(5)').click(function(){
+            $('.changebox5').css("opacity", "1");
+            $('.changebox1').css("opacity", "0");
+            $('.changebox2').css("opacity", "0");
+            $('.changebox3').css("opacity", "0");
+            $('.changebox4').css("opacity", "0");
+        })
     });
---
+++ js/two_section_goods_merch.js
@@ -0,0 +1,85 @@
+
+
+
+    $(function(){
+        $('.new_merch_product_one:nth-of-type(1)').mouseover(function(){
+            $('.details1').fadeIn();
+        })
+
+        $('.new_merch_product_one:nth-of-type(1)').mouseleave(function(){
+            $('.details1').fadeOut();
+        })
+
+        $('.new_merch_product_one:nth-of-type(2)').mouseover(function(){
+            $('.details2').fadeIn();
+        })
+
+        $('.new_merch_product_one:nth-of-type(2)').mouseleave(function(){
+            $('.details2').fadeOut();
+        })
+
+        $('.new_merch_product_one:nth-of-type(3)').mouseover(function(){
+            $('.details3').fadeIn();
+        })
+
+        $('.new_merch_product_one:nth-of-type(3)').mouseleave(function(){
+            $('.details3').fadeOut();
+        })
+
+        $('.new_merch_product_one:nth-of-type(4)').mouseover(function(){
+            $('.details4').fadeIn();
+        })
+
+        $('.new_merch_product_one:nth-of-type(4)').mouseleave(function(){
+            $('.details4').fadeOut();
+        })
+
+        $('.new_merch_product_one:nth-of-type(5)').mouseover(function(){
+            $('.details5').fadeIn();
+        })
+
+        $('.new_merch_product_one:nth-of-type(5)').mouseleave(function(){
+            $('.details5').fadeOut();
+        })
+
+        $('.acc_product_one:nth-of-type(1)').mouseover(function(){
+            $('.details6').fadeIn();
+        })
+
+        $('.acc_product_one:nth-of-type(1)').mouseleave(function(){
+            $('.details6').fadeOut();
+        })
+
+        $('.acc_product_one:nth-of-type(2)').mouseover(function(){
+            $('.details7').fadeIn();
+        })
+
+        $('.acc_product_one:nth-of-type(2)').mouseleave(function(){
+            $('.details7').fadeOut();
+        })
+
+        $('.acc_product_one:nth-of-type(3)').mouseover(function(){
+            $('.details8').fadeIn();
+        })
+
+        $('.acc_product_one:nth-of-type(3)').mouseleave(function(){
+            $('.details8').fadeOut();
+        })
+
+        $('.acc_product_one:nth-of-type(4)').mouseover(function(){
+            $('.details9').fadeIn();
+        })
+
+        $('.acc_product_one:nth-of-type(4)').mouseleave(function(){
+            $('.details9').fadeOut();
+        })
+
+        $('.acc_product_one:nth-of-type(5)').mouseover(function(){
+            $('.details10').fadeIn();
+        })
+
+        $('.acc_product_one:nth-of-type(5)').mouseleave(function(){
+            $('.details10').fadeOut();
+        })
+
+    });
