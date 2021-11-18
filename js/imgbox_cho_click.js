83cf1e2810becadb8bdce5263a7e3f6ab9ee5822
fe2oz <93234748+fe2oz@users.noreply.github.com>
2021-11-18 18:01:57 +0900

    11.18/18:01

--- js/imgbox_cho_click.js
+++ js/imgbox_cho_click.js
@@ -2,10 +2,23 @@ $(function(){
     $('.imgbox_cho li').click(function(){
         $('.imgbox_cho li').removeClass("active");
         $(this).addClass("active");
-    });
+    })
 
     $('.imgbox_cho li').click(function(){
         $('.imgbox_cho li').removeClass("active");
         $(this).addClass("active");
-    });
+    })
+
+});
+
+$(function(){
+    $('.imgbox_cho li:nth-of-type(1)').click(function(){
+        $('.cho1').fadeIn();
+        $('.cho2').fadeOut();
+    })
+
+    $('.imgbox_cho li:nth-of-type(2)').click(function(){
+        $('.cho2').fadeIn();
+        $('.cho1').fadeOut();
+    })
 });
