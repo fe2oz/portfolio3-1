4e6a4a1a443070d6cf5be0652f6b9a98986543d9
fe2oz <93234748+fe2oz@users.noreply.github.com>
2021-11-23 17:48:14 +0900

    11.23/17:48

--- js/sub1_news.js
+++ js/sub1_news.js
@@ -17,12 +17,28 @@ $(function(){
         $('.newstext2').css("opacity", "0");
     });
 
+    $('.photo3').mouseover(function(){
+        $('.newstext3').css("opacity", "1");
+    });
+
+    $('.photo3').mouseleave(function(){
+        $('.newstext3').css("opacity", "0");
+    });
+
     $('.photo4').mouseover(function(){
         $('.newstext4').css("opacity", "1");
     });
 
     $('.photo4').mouseleave(function(){
         $('.newstext4').css("opacity", "0");
+    });
+
+    $('.photo5').mouseover(function(){
+        $('.newstext5').css("opacity", "1");
+    });
+
+    $('.photo5').mouseleave(function(){
+        $('.newstext5').css("opacity", "0");
     });
 
     $('.photo6').mouseover(function(){
