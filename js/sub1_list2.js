c9b8ec94a95b4d1c73a349814827c087e24b7f9c
fe2oz <93234748+fe2oz@users.noreply.github.com>
2021-11-23 17:41:33 +0900

    11.23/17:41

---
+++ js/sub1_list2.js
@@ -0,0 +1,308 @@
+
+
+
+$(function(){
+    $('.list2:nth-of-type(1)').mouseover(function(){
+        $('.iwt1').fadeIn();
+    });
+
+    $('.list2:nth-of-type(1)').mouseleave(function(){
+        $('.iwt1').fadeOut();
+    });
+
+    $('.list2:nth-of-type(2)').mouseover(function(){
+        $('.iwt2').fadeIn();
+    });
+
+    $('.list2:nth-of-type(2)').mouseleave(function(){
+        $('.iwt2').fadeOut();
+    });
+
+    $('.list2:nth-of-type(3)').mouseover(function(){
+        $('.iwt3').fadeIn();
+    });
+
+    $('.list2:nth-of-type(3)').mouseleave(function(){
+        $('.iwt3').fadeOut();
+    });
+
+    $('.list2:nth-of-type(4)').mouseover(function(){
+        $('.iwt4').fadeIn();
+    });
+
+    $('.list2:nth-of-type(4)').mouseleave(function(){
+        $('.iwt4').fadeOut();
+    });
+
+    $('.list2:nth-of-type(5)').mouseover(function(){
+        $('.iwt5').fadeIn();
+    });
+
+    $('.list2:nth-of-type(5)').mouseleave(function(){
+        $('.iwt5').fadeOut();
+    });
+
+    $('.list2:nth-of-type(6)').mouseover(function(){
+        $('.iwt6').fadeIn();
+    });
+
+    $('.list2:nth-of-type(6)').mouseleave(function(){
+        $('.iwt6').fadeOut();
+    });
+
+    $('.list2:nth-of-type(7)').mouseover(function(){
+        $('.iwt7').fadeIn();
+    });
+
+    $('.list2:nth-of-type(7)').mouseleave(function(){
+        $('.iwt7').fadeOut();
+    });
+
+    $('.list2:nth-of-type(8)').mouseover(function(){
+        $('.iwt8').fadeIn();
+    });
+
+    $('.list2:nth-of-type(8)').mouseleave(function(){
+        $('.iwt8').fadeOut();
+    });
+
+    $('.list2:nth-of-type(9)').mouseover(function(){
+        $('.iwt9').fadeIn();
+    });
+
+    $('.list2:nth-of-type(9)').mouseleave(function(){
+        $('.iwt9').fadeOut();
+    });
+
+    $('.list2:nth-of-type(10)').mouseover(function(){
+        $('.iwt10').fadeIn();
+    });
+
+    $('.list2:nth-of-type(10)').mouseleave(function(){
+        $('.iwt10').fadeOut();
+    });
+
+    $('.list2:nth-of-type(11)').mouseover(function(){
+        $('.iwt11').fadeIn();
+    });
+
+    $('.list2:nth-of-type(11)').mouseleave(function(){
+        $('.iwt11').fadeOut();
+    });
+
+    $('.list2:nth-of-type(12)').mouseover(function(){
+        $('.iwt12').fadeIn();
+    });
+
+    $('.list2:nth-of-type(12)').mouseleave(function(){
+        $('.iwt12').fadeOut();
+    });
+
+    $('.list2:nth-of-type(13)').mouseover(function(){
+        $('.iwt13').fadeIn();
+    });
+
+    $('.list2:nth-of-type(13)').mouseleave(function(){
+        $('.iwt13').fadeOut();
+    });
+
+    $('.list2:nth-of-type(14)').mouseover(function(){
+        $('.iwt14').fadeIn();
+    });
+
+    $('.list2:nth-of-type(14)').mouseleave(function(){
+        $('.iwt14').fadeOut();
+    });
+
+    $('.list2:nth-of-type(15)').mouseover(function(){
+        $('.iwt15').fadeIn();
+    });
+
+    $('.list2:nth-of-type(15)').mouseleave(function(){
+        $('.iwt15').fadeOut();
+    });
+
+    $('.list2:nth-of-type(16)').mouseover(function(){
+        $('.iwt16').fadeIn();
+    });
+
+    $('.list2:nth-of-type(16)').mouseleave(function(){
+        $('.iwt16').fadeOut();
+    });
+
+    $('.list2:nth-of-type(17)').mouseover(function(){
+        $('.iwt17').fadeIn();
+    });
+
+    $('.list2:nth-of-type(17)').mouseleave(function(){
+        $('.iwt17').fadeOut();
+    });
+
+    $('.list2:nth-of-type(18)').mouseover(function(){
+        $('.iwt18').fadeIn();
+    });
+
+    $('.list2:nth-of-type(18)').mouseleave(function(){
+        $('.iwt18').fadeOut();
+    });
+
+    $('.list2:nth-of-type(19)').mouseover(function(){
+        $('.iwt19').fadeIn();
+    });
+
+    $('.list2:nth-of-type(19)').mouseleave(function(){
+        $('.iwt19').fadeOut();
+    });
+
+    $('.list2:nth-of-type(20)').mouseover(function(){
+        $('.iwt20').fadeIn();
+    });
+
+    $('.list2:nth-of-type(20)').mouseleave(function(){
+        $('.iwt20').fadeOut();
+    });
+
+    $('.list2:nth-of-type(21)').mouseover(function(){
+        $('.iwt21').fadeIn();
+    });
+
+    $('.list2:nth-of-type(21)').mouseleave(function(){
+        $('.iwt21').fadeOut();
+    });
+
+    $('.list2:nth-of-type(22)').mouseover(function(){
+        $('.iwt22').fadeIn();
+    });
+
+    $('.list2:nth-of-type(22)').mouseleave(function(){
+        $('.iwt22').fadeOut();
+    });
+
+    $('.list2:nth-of-type(23)').mouseover(function(){
+        $('.iwt23').fadeIn();
+    });
+
+    $('.list2:nth-of-type(23)').mouseleave(function(){
+        $('.iwt23').fadeOut();
+    });
+
+    $('.list2:nth-of-type(24)').mouseover(function(){
+        $('.iwt24').fadeIn();
+    });
+
+    $('.list2:nth-of-type(24)').mouseleave(function(){
+        $('.iwt24').fadeOut();
+    });
+
+    $('.list2:nth-of-type(25)').mouseover(function(){
+        $('.iwt25').fadeIn();
+    });
+
+    $('.list2:nth-of-type(25)').mouseleave(function(){
+        $('.iwt25').fadeOut();
+    });
+
+    $('.list2:nth-of-type(26)').mouseover(function(){
+        $('.iwt26').fadeIn();
+    });
+
+    $('.list2:nth-of-type(26)').mouseleave(function(){
+        $('.iwt26').fadeOut();
+    });
+
+    $('.list2:nth-of-type(27)').mouseover(function(){
+        $('.iwt27').fadeIn();
+    });
+
+    $('.list2:nth-of-type(27)').mouseleave(function(){
+        $('.iwt27').fadeOut();
+    });
+
+    $('.list2:nth-of-type(28)').mouseover(function(){
+        $('.iwt28').fadeIn();
+    });
+
+    $('.list2:nth-of-type(28)').mouseleave(function(){
+        $('.iwt28').fadeOut();
+    });
+
+    $('.list2:nth-of-type(29)').mouseover(function(){
+        $('.iwt29').fadeIn();
+    });
+
+    $('.list2:nth-of-type(29)').mouseleave(function(){
+        $('.iwt29').fadeOut();
+    });
+
+    $('.list2:nth-of-type(30)').mouseover(function(){
+        $('.iwt30').fadeIn();
+    });
+
+    $('.list2:nth-of-type(30)').mouseleave(function(){
+        $('.iwt30').fadeOut();
+    });
+
+    $('.list2:nth-of-type(31)').mouseover(function(){
+        $('.iwt31').fadeIn();
+    });
+
+    $('.list2:nth-of-type(31)').mouseleave(function(){
+        $('.iwt31').fadeOut();
+    });
+
+    $('.list2:nth-of-type(32)').mouseover(function(){
+        $('.iwt32').fadeIn();
+    });
+
+    $('.list2:nth-of-type(32)').mouseleave(function(){
+        $('.iwt32').fadeOut();
+    });
+
+    $('.list2:nth-of-type(33)').mouseover(function(){
+        $('.iwt33').fadeIn();
+    });
+
+    $('.list2:nth-of-type(33)').mouseleave(function(){
+        $('.iwt33').fadeOut();
+    });
+
+    $('.list2:nth-of-type(34)').mouseover(function(){
+        $('.iwt34').fadeIn();
+    });
+
+    $('.list2:nth-of-type(34)').mouseleave(function(){
+        $('.iwt34').fadeOut();
+    });
+
+    $('.list2:nth-of-type(35)').mouseover(function(){
+        $('.iwt35').fadeIn();
+    });
+
+    $('.list2:nth-of-type(35)').mouseleave(function(){
+        $('.iwt35').fadeOut();
+    });
+
+    $('.list2:nth-of-type(36)').mouseover(function(){
+        $('.iwt36').fadeIn();
+    });
+
+    $('.list2:nth-of-type(36)').mouseleave(function(){
+        $('.iwt36').fadeOut();
+    });
+
+    $('.list2:nth-of-type(37)').mouseover(function(){
+        $('.iwt37').fadeIn();
+    });
+
+    $('.list2:nth-of-type(37)').mouseleave(function(){
+        $('.iwt37').fadeOut();
+    });
+
+    $('.list2:nth-of-type(38)').mouseover(function(){
+        $('.iwt38').fadeIn();
+    });
+
+    $('.list2:nth-of-type(38)').mouseleave(function(){
+        $('.iwt38').fadeOut();
+    });
+});
---
+++ js/sub1_news.js
@@ -0,0 +1,35 @@
+
+
+$(function(){
+    $('.photo1').mouseover(function(){
+        $('.newstext1').css("opacity", "1");
+    });
+
+    $('.photo1').mouseleave(function(){
+        $('.newstext1').css("opacity", "0");
+    });
+
+    $('.photo2').mouseover(function(){
+        $('.newstext2').css("opacity", "1");
+    });
+
+    $('.photo2').mouseleave(function(){
+        $('.newstext2').css("opacity", "0");
+    });
+
+    $('.photo4').mouseover(function(){
+        $('.newstext4').css("opacity", "1");
+    });
+
+    $('.photo4').mouseleave(function(){
+        $('.newstext4').css("opacity", "0");
+    });
+
+    $('.photo6').mouseover(function(){
+        $('.newstext6').css("opacity", "1");
+    });
+
+    $('.photo6').mouseleave(function(){
+        $('.newstext6').css("opacity", "0");
+    });
+});
